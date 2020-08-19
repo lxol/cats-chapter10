26db54ef7706bd7f669670bdcd955bd8a0ac334c
Alex Olkhovskiy <lxol@users.noreply.github.com>
2020-08-19 08:51:52 +0100

    refactor to use Validated instead of Either

--- src/main/scala/sandbox/Chapter10.scala
+++ src/main/scala/sandbox/Chapter10.scala
@@ -1,28 +1,29 @@
 package sandbox
 
+import cats.data.Validated
 import cats.kernel.Semigroup
 import cats.syntax.all._
 
 object Chapter10 {
   trait Check[E, A] {
     import Check._
-    def apply(value: A)(implicit E: Semigroup[E]): Either[E, A] = this match {
+    def apply(value: A)(implicit E: Semigroup[E]): Validated[E, A] = this match {
       case (Pure(f)) => f(value)
-      case (And(c1, c2)) =>
-        (c1(value), c2(value)) match {
-          case (Right(form), Right(_)) => form.asRight[E]
-          case (Left(e), Right(_))     => e.asLeft[A]
-          case (Right(_), Left(e))     => e.asLeft[A]
-          case (Left(e1), Left(e2))    => (e1 |+| e2).asLeft[A]
-        }
+      case (And(c1, c2)) => (c1(value), c2(value)).mapN((c,_) => c)
+//        (c1(value), c2(value)) match {
+//          case (Valid(form), Valid(_)) => form.valid[E]
+//          case (Invalid(e), Valid(_))     => e.invalid[A]
+//          case (Valid(_), Invalid(e))     => e.invalid[A]
+//          case (Invalid(e1), Invalid(e2))    => (e1 |+| e2).invalid[A]
+//        }
     }
   }
 
   object Check {
-    final case class Pure[E, A](a: A => Either[E, A]) extends Check[E, A]
+    final case class Pure[E, A](a: A => Validated[E, A]) extends Check[E, A]
     final case class And[E, A](c1: Check[E, A], c2: Check[E, A])
         extends Check[E, A]
-    def pure[E, A](f: A => Either[E, A]) = Pure(f)
+    def pure[E, A](f: A => Validated[E, A]) = Pure(f)
   }
 
 }
@@ -37,15 +38,15 @@ object Fixtures {
 
   val check1 = pure(
     (form: MyForm) =>
-      if (form.name.size > 0) form.asRight[List[String]]
-      else List("empty name").asLeft[MyForm]
+      if (form.name.size > 0) form.valid[List[String]]
+      else List("empty name").invalid[MyForm]
   )
 
   val check2 = pure(
     (form: MyForm) =>
-      if (form.name.size > 0) form.asRight[List[String]]
-      else List("empty name").asLeft[MyForm]
+      if (form.password.size > 0) form.valid[List[String]]
+      else List("empty name").invalid[MyForm]
   )
 
-  val both1And2 =  And(check1, check2)
+  val both1And2 = And(check1, check2)
 }
